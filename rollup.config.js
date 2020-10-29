54f1859a00b318585d95274ce0efc1383503ef15
mr-rampage <readonly@che.wbac.ca>
2020-10-29 17:33:02 -0400

    :recycle: Removed Webpack

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,10 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import json from '@rollup/plugin-json';
 import commonjs from '@rollup/plugin-commonjs';
+import babel from '@rollup/plugin-babel';
 import nodePolyfills from 'rollup-plugin-node-polyfills';
-import babel from 'rollup-plugin-babel';
 import copy from 'rollup-plugin-copy';
+import { terser } from 'rollup-plugin-terser';
 
 export default {
   input: 'src/main.js',
@@ -20,13 +21,23 @@ export default {
     }),
     json(),
     nodePolyfills(),
-    babel(),
+    babel({
+      babelHelpers: 'bundled',
+      presets: [
+        ['@babel/preset-env', {
+          targets: {
+            browsers: ['last 2 versions'],
+          },
+        }],
+      ],
+    }),
     copy({
       targets: [
         { src: 'node_modules/itk/WebWorkers', dest: 'public/itk' },
         { src: 'node_modules/itk/ImageIOs', dest: 'public/itk' },
         { src: 'node_modules/itk/MeshIOs', dest: 'public/itk' },
       ]
-    })
+    }),
+    terser()
   ]
 }
